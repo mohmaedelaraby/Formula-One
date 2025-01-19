83a90082771b46ed5ca4c4018f59c3e84536faa6
mohmaedelaraby <elarabym857@gmail.com>
2025-01-20 01:51:35 +0200

    race detials file

--- src/system/modules/races/RacesPerSeason.tsx
+++ src/system/modules/races/RacesPerSeason.tsx
@@ -78,7 +78,7 @@ function RacesPerSeason() {
                 <TableRow hover key={index}>
                   <TableCell
                     onClick={() => {
-                      navigate(`/${season.season}/races`);
+                      navigate(`/${season.season}/${season.round}/results`);
                     }}
                   >
                     {season.season}
